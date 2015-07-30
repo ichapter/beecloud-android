949d7fba23d59c9798f272219b0bb7fff09f72dc
xuanzhui <daiwei1215@126.com>
2015-07-30 20:50:29 +0800

    更换app id和app secret

--- demo/src/main/java/cn/beecloud/sdk_demo/ShoppingCartActivity.java
+++ demo/src/main/java/cn/beecloud/sdk_demo/ShoppingCartActivity.java
@@ -68,9 +68,8 @@ public class ShoppingCartActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
 
-        // 推荐在主Activity里的onCreate函数中初始化BeeCloud.  c37d661d-7e61-49ea-96a5-68c34e83db3b
-        //BeeCloud.setAppIdAndSecret("c5d1cba1-5e3f-4ba0-941d-9b0a371fe719", "39a7a518-9ac8-4a9e-87bc-7885f33cf18c");
-        BeeCloud.setAppIdAndSecret("c37d661d-7e61-49ea-96a5-68c34e83db3b", "c37d661d-7e61-49ea-96a5-68c34e83db3b");
+        // 推荐在主Activity里的onCreate函数中初始化BeeCloud.
+        BeeCloud.setAppIdAndSecret("c5d1cba1-5e3f-4ba0-941d-9b0a371fe719", "39a7a518-9ac8-4a9e-87bc-7885f33cf18c");
 
         // 如果用到微信支付，在用到微信支付的Activity的onCreate函数里调用以下函数.
         // 第二个参数需要换成你自己的微信AppID.
